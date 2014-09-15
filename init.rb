9019c3dac28a2103702dd6200e9cd6a90c0e5d13
Damir Konkov <d4mk0f@gmail.com>
2014-09-15 19:44:06 +0400

    initial commit for plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :result_field do
+  name 'Result Field plugin'
+  author 'Damir Konkov'
+  description 'This is a plugin for Redmine for creating additional field to tasks named Result'
+  version '0.0.1'
+  url 'https://github.com/d4mk0/redmine_result_field'
+  author_url 'https://github.com/d4mk0'
+end
